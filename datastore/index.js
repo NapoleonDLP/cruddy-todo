33eec9a62ab3586b094b0b322ac29fae49b95016
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-18 10:56:19 -0700

    Passed specs for read all with id as both args

--- datastore/index.js
+++ datastore/index.js
@@ -22,11 +22,50 @@ exports.create = (text, callback) => {
   });
 };
 
+//read dataDir, build list of files
+//NOTE: id of each todo item is encoded in its filename
+//include texxt field in response to client
+  //pass in message id for both id field and text content
+
+/*
+i: callback
+o:
+
+NOTE:
+- readdir returns array of
+- path: exports.dataDir
+*/
+
 exports.readAll = (callback) => {
-  var data = _.map(items, (text, id) => {
-    return { id, text };
+  //initialize empty data array
+  var data = [];
+
+
+  fs.readdir(exports.dataDir, (err, files) => {
+    if (err) {
+      //callback?
+      //throw?
+    } else {
+      files.forEach(id => {
+        data.push({ id: id.substring(0, id.length - 4), text: id.substring(0, id.length - 4) });
+      });
+      callback(null, data);
+    }
   });
-  callback(null, data);
+  //read directory -- readdir(path, callback(err, files)) {
+    //err first
+      //throw error?
+      //callback(err)?
+    //no error
+      //iterate over files (forEach?)
+        //files.forEach(fileName => {data.push(yadayada)})
+        //push each file as an object into array
+        //callback(null, data)
+
+  // var data = _.map(items, (text, id) => {
+  //   return { id, text };
+  // });
+  // callback(null, data);
 };
 
 exports.readOne = (id, callback) => {
